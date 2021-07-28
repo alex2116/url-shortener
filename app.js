0c65ac78bf71e6ba077d6ea0216ae04a39620e3b
alex2116 <reingeld116@gmail.com>
2021-07-28 12:00:21 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const routes = require('./routes')
 
+const PORT = process.env.PORT || 3000
+
 require('./config/mongoose')
 
 const app = express()
@@ -14,8 +16,8 @@ app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log('App is running on port 3000')
+app.listen(PORT, () => {
+  console.log('App is running on http://localhost:${PORT}')
 })
 
 
